57a64c5db000aad457d69b6e0bf71183f8e82ae7
Dan Seminara <japaneseforgaming@gmail.com>
2021-11-06 18:44:44 -0400

    Peer grading continued

--- src/components/PeerGrading.js
+++ src/components/PeerGrading.js
@@ -18,7 +18,7 @@ function PeerGrading({url, currentUser}) {
 
 
     function processData(data) {
-        
+            let useTheseQuestions = []
             usersQuiz = Object.keys(currentUser.quizzes).length
             usersQuiz -= 1
             if (usersQuiz === 0) { usersQuiz = 1 }
@@ -33,10 +33,19 @@ function PeerGrading({url, currentUser}) {
                      }
                  }
             }
-            console.log(frQuestions)
-            setGradingQuestions(frQuestions)
-            toGrade = frQuestions.map((i, j) => { 
-                let name = "peerGrade" + j
+            for (let i = 0; i < 7; i++){
+                let rando = (Math.floor(Math.random() * frQuestions.length))
+                if (useTheseQuestions.includes(frQuestions[rando])) {
+                    i-=1
+                    continue
+                 }
+                 useTheseQuestions.push(frQuestions[rando])
+            }
+
+            console.log(useTheseQuestions)
+            setGradingQuestions(useTheseQuestions)
+            toGrade = useTheseQuestions.map((i, j) => { 
+                //let name = "peerGrade" + j
                 //console.log(name)
                 return ( 
                         <div className="peerGrading">
@@ -46,7 +55,7 @@ function PeerGrading({url, currentUser}) {
                             </div>
                              <div className="peerQuestionChoice" id={j}>
                              <form className="selectionForm" >
-                                <input type="radio" value="Correct" name={j} name="a" id="correct" /> Correct<br />
+                                <input type="radio" value="Correct" name="a" name="a" id="correct" /> Correct<br />
                                 <input type="radio" value="Incorrect" name="a" id="incorrect" /> Incorrect<br />
                                 <input type="radio" value="Skip" name="a" id="skip" /> Skip
                              </form>
@@ -66,15 +75,13 @@ function PeerGrading({url, currentUser}) {
         function sumbitForms(e) {
                 e.preventDefault()
                 let eachChoice=[]
-                let tempArray = []
                 let choices = document.querySelectorAll(".selectionForm input")
                 let arrayOfRadios = [...choices]
                 let i = 0
                 while (arrayOfRadios[2]) {
-                    tempArray = [arrayOfRadios[0], arrayOfRadios[1], arrayOfRadios[2]]
-                    if (tempArray[0].checked === true) { eachChoice.push("Correct") }
-                    else if (tempArray[1].checked === true) { eachChoice.push("Incorrect") }
-                    else if (tempArray[2].checked === true) { eachChoice.push("skipped") }
+                    if (arrayOfRadios[0].checked === true) { eachChoice.push("Correct") }
+                    else if (arrayOfRadios[1].checked === true) { eachChoice.push("Incorrect") }
+                    else if (arrayOfRadios[2].checked === true) { eachChoice.push("skipped") }
                     else { 
                         document.querySelector(".error").innerHTML="Please make choices for all the questions before you submit."
                         break
